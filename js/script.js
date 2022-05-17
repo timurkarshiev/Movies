25aa055bc6f122c008a527470e009f8d57d7085e
Timur Karshiev <103237264+timurkarshiev@users.noreply.github.com>
2022-05-17 11:22:50 +0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// const headerBtn = document.querySelector (".header__btn");
+// const elHeader = document.querySelector('.header')
+// let headerOpen = false;
+
+
+// headerBtn.addEventListener("click", () => {
+//    if(!headerOpen) {
+//       headerBtn.classList.add('open');
+//       elHeader.classList.add('open-header');
+//       headerOpen = true;
+//    } else {
+//       headerBtn.classList.remove('open');
+//       elHeader.classList.remove('open-header');
+//       headerOpen = false;
+//    }
+// });
